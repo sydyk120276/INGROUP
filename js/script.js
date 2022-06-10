9f09e137648317debf3cd44d356cff821725b6d2
sydyk120276 <akbar@github.com>
2022-06-10 13:47:21 +0600

    Адаптив 2

--- js/script.js
+++ js/script.js
@@ -22,13 +22,54 @@ $(".slider").slick({
 });
 
 
-const list = document.querySelectorAll(".nav-link")
-list.forEach(item => {
-  item.addEventListener('click', (e) => {
-    list.forEach(el => { el.classList.remove('active'); });
-    item.classList.add('active')
-  })
-})
+// $(".header-link").click(function () {
+//   $(".header-link").removeClass("active");
+//   $(this).addClass("active");
+//   return false;
+// });
+
+// var menuItems = document.getElementsByClassName("header-link");
+// var onClick = function (event) {
+//   event.preventDefault();
+
+//   for (var i = 0; i < menuItems.length; i++) {
+//     menuItems[i].classList.remove("active");
+//   }
+
+//   event.currentTarget.classList.add("active");
+// };
+
+// for (var i = 0; i < menuItems.length; i++) {
+//   menuItems[i].addEventListener("click", onClick, false);
+// }
+
+// window.addEventListener("load", function () {
+//   var elem = document
+//     .getElementsByClassName("header-link")[0]
+//     .getElementsByTagName("a");
+//   for (var i = 0; i < elem.length; i++)
+//     elem[i].addEventListener("click", function () {
+//       localStorage.setItem("a", this.textContent);
+//     });
+//   for (var i = 0; i < elem.length; i++)
+//     if (elem[i].textContent == localStorage.getItem("a"))
+//       elem[i].classList.add("active");
+// });
+
+// var navLink = $(".header-link").on("click", function (e) {
+//   e.preventDefault();
+//   navLink.not(this).removeClass("active");
+//   $(this).addClass("active");
+// });
+
+// const list = document.querySelectorAll(".header-link");
+// console.log(list)
+// list.forEach(item => {
+//   item.addEventListener('click', (e) => {
+//     list.forEach(el => { el.classList.remove('active'); });
+//     item.classList.add('active')
+//   })
+// })
 
 document.querySelector(".burger-block").addEventListener('click', function () {
   document.querySelector(".burger").classList.toggle("active")
